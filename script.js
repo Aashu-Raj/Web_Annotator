e599f1cc4c19419fea4be8b74b11e80601d6f251
Aashu Raj <146524948+AashuRajIITR@users.noreply.github.com>
2024-06-09 23:46:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('enable-annotator').addEventListener('click', () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        files: ['content.js'],
+      });
+    });
+  });
+  
